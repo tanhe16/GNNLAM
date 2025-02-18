e78030c6c9d560d606fdc215de78f4cf5a8aab8f
tanhe16 <tanhe.imust@foxmail.com>
2025-02-18 22:20:18 +0800

    Add files via upload

--- opt.py
+++ opt.py
@@ -2,30 +2,27 @@ from clr import cyclic_learning_rate
 import tensorflow as tf
 
 
-# Optimizer 类：这个类负责为模型定义优化器。
-# 它接受多个参数，包括模型本身（model）、预测值（preds）、标签（labels）、学习率（lr）、num_u=269行--drug数量、num_v=598列--disease数量、以及关联数量（association_nam）。
+
 class Optimizer():
     def __init__(self, model, preds, labels, lr, num_u, num_v, association_nam):
-        norm = num_u*num_v / float((num_u*num_v-association_nam) * 2)  # 计算权重正则化因子
-        # 获取预测值和标签
+        norm = num_u*num_v / float((num_u*num_v-association_nam) * 2)
+
         preds_sub = preds
         labels_sub = labels
 
-        # 计算正样本权重
         pos_weight = float(num_u*num_v-association_nam)/(association_nam)
-        global_step = tf.Variable(0, trainable=False)  # 定义全局步数变量
+        global_step = tf.Variable(0, trainable=False)
 
-        # 使用Adam优化器，设置学习率----------------采用的是循环学习率，clr.py，使学习率在最大学习率和最小学习率之间发生变化，帮助我们平衡训练速度和ACC。
         self.optimizer = tf.train.AdamOptimizer(learning_rate=cyclic_learning_rate(global_step=global_step, learning_rate=lr*0.1,
 
-                                                                                   max_lr=lr, mode='exp_range', gamma=.99)) #0.995--0.99
-        # 计算损失函数---------加权交叉熵----------对应的是公式（11）
+                                                                                   max_lr=lr, mode='exp_range', gamma=.99))
+
         self.cost = norm * tf.reduce_mean(
             tf.nn.weighted_cross_entropy_with_logits(
                 logits=preds_sub, targets=labels_sub, pos_weight=pos_weight))
 
-        # 使用优化器最小化损失函数
+
         self.opt_op = self.optimizer.minimize(
             self.cost, global_step=global_step,)
-        # 计算梯度
+
         self.grads_vars = self.optimizer.compute_gradients(self.cost)
--- utils.py
+++ utils.py
@@ -3,8 +3,7 @@ import tensorflow as tf
 import scipy.sparse as sp
 
 
-# 这个函数用于初始化权重，采用了Xavier/Glorot初始化方法。
-# 它接受输入维度和输出维度，并返回一个根据 Glorot 初始化方法初始化的权重变量。
+
 def weight_variable_glorot(input_dim, output_dim, name=""):
     init_range = np.sqrt(6.0/(input_dim + output_dim))
     initial = tf.random_uniform(
@@ -17,9 +16,7 @@ def weight_variable_glorot(input_dim, output_dim, name=""):
     return tf.Variable(initial, name=name)
 
 
-# 这个函数实现了对稀疏张量进行dropout操作。
-# 它接受稀疏输入张量 x、保留的元素比例 keep_prob 和张量中非零元素的数量 num_nonzero_elems。
-# 在这个函数中，根据 keep_prob 计算一个随机张量，并将其转换为布尔类型的dropout掩码，然后使用 tf.sparse_retain 函数应用dropout操作。
+
 def dropout_sparse(x, keep_prob, num_nonzero_elems):
     noise_shape = [num_nonzero_elems]
     random_tensor = keep_prob
@@ -29,9 +26,7 @@ def dropout_sparse(x, keep_prob, num_nonzero_elems):
     return pre_out*(1./keep_prob)
 
 
-# 这个函数将稀疏矩阵表示转换为元组表示。
-# 如果输入不是COO格式的稀疏矩阵，则将其转换为COO格式。
-# 最后，返回元组 (coords, values, shape)，其中 coords 是非零元素的坐标，values 是非零元素的值，shape 是矩阵的形状。
+
 def sparse_to_tuple(sparse_mx):
     if not sp.isspmatrix_coo(sparse_mx):
         sparse_mx = sparse_mx.tocoo()
@@ -41,8 +36,7 @@ def sparse_to_tuple(sparse_mx):
     return coords, values, shape
 
 
-# 这个函数对输入的邻接矩阵进行预处理。
-# 首先将邻接矩阵转换为COO格式，然后计算每行的和，接着根据度矩阵的倒数的平方来归一化邻接矩阵，最后将归一化后的邻接矩阵转换为元组表示。
+
 def preprocess_graph(adj):
     adj_ = sp.coo_matrix(adj)
     rowsum = np.array(adj_.sum(1))
@@ -53,10 +47,6 @@ def preprocess_graph(adj):
     return sparse_to_tuple(adj_nomalized)
 
 
-# 这个函数根据给定的药物-疾病关联矩阵构建一个完整的图结构的邻接矩阵。
-# 首先创建两个全零矩阵，分别表示药物和疾病的邻接矩阵。
-# 然后将给定的药物-疾病关联矩阵水平堆叠起来，得到一个 mat1。
-# 将 mat1 转置后与全零矩阵垂直堆叠，得到完整的邻接矩阵 adj
 def constructNet(drug_dis_matrix):
     drug_matrix = np.matrix(
         np.zeros((drug_dis_matrix.shape[0], drug_dis_matrix.shape[0]), dtype=np.int8))
@@ -69,13 +59,7 @@ def constructNet(drug_dis_matrix):
     return adj
 
 
-# 这个函数与 constructNet 函数类似，不过它接受额外的药物和疾病的邻接矩阵作为参数。
-# 它将给定的药物-疾病关联矩阵与额外的药物和疾病的邻接矩阵进行水平和垂直堆叠，构建一个更复杂的图结构的邻接矩阵。
 def constructHNet(drug_dis_matrix, drug_matrix, dis_matrix):
     mat1 = np.hstack((drug_matrix, drug_dis_matrix))
     mat2 = np.hstack((drug_dis_matrix.T, dis_matrix))
     return np.vstack((mat1, mat2))
-
-
-# 这些函数主要用于构建和处理图数据，其中 constructNet 和 constructHNet 函数直接体现了图结构的构建过程，
-# 而其他函数则为构建过程提供了必要的辅助功能，如权重初始化、dropout操作和稀疏矩阵的处理。
